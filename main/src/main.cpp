bd104ba39f60803be6fbef095a09dcf9b9573733
Stephen Gutknecht <stephen.gutkneht@gmail.com>
2016-11-16 01:04:08 -0500

    rectangle a little larger, draw second color

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -35,23 +35,30 @@ int main(int argc, char* argv[]) {
     // Clear winow
     SDL_RenderClear( renderer );
 
-    // Creat a rect at pos ( 50, 50 ) that's 50 pixels wide and 50 pixels high.
+    // Create a rect at pos ( 50, 50 ) that's 80 pixels wide and 80 pixels high.
     SDL_Rect r;
     r.x = 50;
     r.y = 50;
-    r.w = 50;
-    r.h = 50;
+    r.w = 80;
+    r.h = 80;
 
     // Set render color to blue ( rect will be rendered in this color )
     SDL_SetRenderDrawColor( renderer, 0, 0, 255, 255 );
-
     // Render rect
     SDL_RenderFillRect( renderer, &r );
-
     // Render the rect to the screen
     SDL_RenderPresent(renderer);
 
-    SDL_Delay(8000);  // Pause execution for 3000 milliseconds, for example
+    SDL_Delay(8000);  // Pause execution
+
+    // Set render color to green
+    SDL_SetRenderDrawColor( renderer, 0, 255, 0, 255 );
+    // Render rect
+    SDL_RenderFillRect( renderer, &r );
+    // Render the rect to the screen
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);  // Pause execution
 
     // Close and destroy the window
     SDL_DestroyWindow(window);
